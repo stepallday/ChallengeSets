e36d8a195a462e8d8f1f793d171a16d97aae9fd6
Stephanie Hicks <stepallday@yahoo.com>
2020-10-29 22:30:04 -0400

    Finished Challenge One

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -5,37 +5,71 @@ namespace ChallengeSets
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            double sub1 = minuend - subtrahend;
+
+            {
+                return sub1;
+            }
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
-
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return factor1 * factor2;
         }
+
+
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
-        }
+            //throw new NotImplementedException();
+            if (nameOfPerson.Length > 0)
+            {
+                return "Hello, " + nameOfPerson + "!";
+            }
+            else
+            {
+                return "Hello! ";
+            }
 
+        }
         public string GetHey()
         {
-            return "HEY!";
+            return"Hey!";
         }
+
+        
     }
 }
