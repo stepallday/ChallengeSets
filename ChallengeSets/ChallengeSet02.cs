71c4d9ae67069cb97d8d892bcbdd2a5442df622f
Stephanie Hicks <stepallday@yahoo.com>
2020-11-11 20:25:06 -0500

    challenge2 completed

--- ChallengeSets/ChallengeSet02.cs
+++ ChallengeSets/ChallengeSet02.cs
@@ -1,58 +1,133 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace ChallengeSets
+namespace ChallengesSets02
 {
-    public class ChallengeSet02
+    public class ChallengesSet02
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return Char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0 ? true : false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            double minValue = 1000.00;
+            double maxValue = 0;
+            foreach (double dub in numbers.ToList())
+            {
+                if (dub < minValue)
+                {
+                    minValue = dub;
+                }
+                if (dub > maxValue)
+                {
+                    maxValue = dub;
+                }
+            }
+           return minValue + maxValue;
         }
-
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return str1.Length < str2.Length ? str1.Length : str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int sum = 0;
+            foreach (int i in numbers)
+            {
+                sum += i;
+            }
+            return sum;
         }
 
-        public int SumEvens(int[] numbers)
+            public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            var sums = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sums += numbers[i];
+                }
+            }
+            return sums;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            var sums = 0;
+            if (numbers == null)
+            {
+                return false;
+            }
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    sums += numbers[i];
+                }
+            }
+            if (sums % 2 == 0)
+            {
+                return false;
+            }
+            return true;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long counter = 0;
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
         }
     }
 }
+
+            
